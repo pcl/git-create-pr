a60c4d1a927dab779577c59de57306d8b6e192a9
Patrick Linskey <plinskey@cisco.com>
2018-05-03 23:58:16 +0200

    add a 'browse' command to open a PR or the list of all PRs

--- test/test_git_pr_create.py
+++ test/test_git_pr_create.py
@@ -26,18 +26,24 @@ class TestGitPRCreate(unittest.TestCase):
             return (404, {}, "Un-tested graphql")
                                                                   
     def setUp(self):
-        responses.add(responses.POST, 'https://github.com/api/v3/repos/pcl/git-pull-request/pulls',
+        responses.add(responses.POST, 'https://api.github.com/repos/pcl/git-pull-request/pulls',
                       json={'number': 7}, status=200)
-        responses.add_callback(responses.POST, 'https://github.com/api/graphql', self.graphql_callback)
-        responses.add(responses.POST, 'https://github.com/api/v3/repos/pcl/git-pull-request/pulls/7/requested_reviewers',
-                      json={'html_url': 'https://github.com/pcl/git-pull-request/pulls/7'}, status=200)
+        responses.add_callback(responses.POST, 'https://api.github.com/graphql', self.graphql_callback)
+        responses.add(responses.POST, 'https://api.github.com/repos/pcl/git-pull-request/pulls/7/requested_reviewers',
+                      json={'html_url': 'https://api.github.com/pcl/git-pull-request/pulls/7'}, status=200)
 
         self.prc = git_pr.PullRequestController()
         self.prc.interactive = False
-        self.prc.configure()
 
     @responses.activate
     def test_create_pr(self):
-        self.assertEqual(('github.com', 'pcl', 'git-pull-request'), self.prc.parse_remote_url('origin'))
+        self.prc.configure()
+
+        self.assertEqual('https://api.github.com', self.prc.rest_api)
+        self.assertEqual('https://api.github.com/graphql', self.prc.graphql_api)
+        self.assertEqual('https://github.com', self.prc.web_url)
+        self.assertEqual('pcl', self.prc.origin_org)
+        self.assertEqual('git-pull-request', self.prc.origin_repo)
+
         self.assertEqual(0, self.prc.create_pr('origin', 'branch', 'pcl'))
         
